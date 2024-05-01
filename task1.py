f691dbd36406fea40e4a973a3067986ac6453fd8
Maxisthatyou <158555539+Maxisthatyou@users.noreply.github.com>
2024-05-01 13:18:26 -0700

    Final

--- task1.py
+++ task1.py
@@ -1,6 +1,16 @@
 def A():
-
+    print("Hello")
 def B():
-
-
+    print("How are you")
 def C():
+    print("Hi")
+
+x = input("Please enter in capital A, B, or C> ")
+if x == "A":
+    A()
+if x == "B":
+    B()
+if x == "C":
+    C()
+else:
+    print("You've entere an incorrect value")
--- task2.py
+++ task2.py
@@ -8,3 +8,25 @@ displays instructions and how to play
 game()
 plays the games
 """
+import random
+
+def title():
+    print("This is a number guessing game. Player will enter a number and try to guess the number the computer chooses between 1 and 100.")
+
+def game():
+    x = random.randint(0, 100)
+    for i in range(100):
+        y = int(input("Please enter an integer between 0 and 100> "))
+        if 0 <= y <= 100:
+            if x == y:
+                print("You've guessed correctly!")
+                break
+            elif x > y:
+                print("The number you've entered is too small, try again.")
+            elif x < y:
+                print("The number you've entered is too big, try again.")
+        else:
+            print("Invalid value, try a number between 0 and 100.")
+
+title()
+game()
